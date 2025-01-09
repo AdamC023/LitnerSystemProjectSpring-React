924fb63331be254f4e4b3d3d46305e45595e2364
adam crawford <adam.crawford.2022@live.rhul.ac.uk>
2025-01-09 18:56:21 +0000

    add card page with post

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,56 @@
-import { useState } from 'react'
-
+import { useEffect,useState } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [card, setCard] = useState({
+      answer: "",
+      question: "",
+    });
+
+  const handleChange = e => {
+      setCard({
+          ...card,
+          [e.target.name]: e.target.value,
+      })
+  }
+
+  const addCard = e => {
+      const addCard = {
+          answer: card.answer,
+          question: card.question,
+      }
+
+      axios.post("http://localhost:2800/cards", addCard)
+          .then(res => {
+              console.log(res)
+              console.log(res.data)
+          })
+          .catch(err => {
+              console.log(err)
+          })
+
+  }
+
+    useEffect(() => {
+        console.log("Updated card:", card);
+    }, [card]);
+
 
   return (
     <>
       <h1>
           Add Card Page
       </h1>
+        <input type="text" name="answer" placeholder="question" onChange={handleChange} />
+
+        <input type="text" name="question" placeholder="answer" onChange={handleChange} />
+        <p>{card.question}</p>
+        <p>{card.answer}</p>
+        <button type="submit" onClick={addCard}>submit</button>
     </>
+
+
   )
 }
 
